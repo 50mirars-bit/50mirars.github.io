195d61b46e32615e63ff13e8052229a40c79e1cd
50mirars-bit <50mirars@gmail.com>
2026-08-23 11:05:45 +0530

    Update products.js

--- products.js
+++ products.js
@@ -44,50 +44,850 @@ const products = [
         link:"product-detailssunglasses.html?id=4"
     },
 
-    /* =====================================================
-       WATCHES
-    ===================================================== */
+   /* =====================================================
+   WATCHES
+===================================================== */
 
-    {
-        id:101,
-        name:"Tommy Hilfiger Clark Watch",
-        brand:"Tommy Hilfiger",
-        category:"Watches",
-        price:"₹3,500",
-        image:"Tommy Hilfiger Clark.jpeg",
-        link:"product-detailswatches.html?id=1"
-    },
+{
+    id:1001,
+    name:"Tissot T-Race Chronograph Watch",
+    brand:"Tissot",
+    category:"Watches",
+    price:"₹3,500",
+    image:"Tissot T-Race Chronograph Watch.jpeg",
+    link:"product-details.html?id=1"
+},
 
-    {
-        id:102,
-        name:"Tommy Hilfiger Dual Tone Watch TH1781644",
-        brand:"Tommy Hilfiger",
-        category:"Watches",
-        price:"₹3,500",
-        image:"Tommy Hilfiger Dual Tone Watch TH1781644.jpeg",
-        link:"product-detailswatches.html?id=2"
-    },
+{
+    id:1002,
+    name:"Tommy Hilfiger Decker Chronograph",
+    brand:"Tommy Hilfiger",
+    category:"Watches",
+    price:"₹4,500",
+    image:"Tommy Hilfiger Decker Chronograph.jpeg",
+    link:"product-details.html?id=71"
+},
 
-    {
-        id:103,
-        name:"Tissot T-Race Chronograph Watch",
-        brand:"Tissot",
-        category:"Watches",
-        price:"₹3,500",
-        image:"Tissot T-Race Chronograph Watch.jpeg",
-        link:"product-detailswatches.html?id=3"
-    },
+{
+    id:1003,
+    name:"Rolex Pheonix Watch",
+    brand:"Rolex",
+    category:"Watches",
+    price:"₹4,000",
+    video:"Rolex Pheonix Watch.mp4",
+    link:"product-details.html?id=2"
+},
 
-    {
-        id:104,
-        name:"Armani Exchange Automatic Skeleton Watch",
-        brand:"Armani Exchange",
-        category:"Watches",
-        price:"₹4,000",
-        image:"Armani Exchange Automatic Skeleton Watch.jpeg",
-        link:"product-detailswatches.html?id=4"
-    },
+{
+    id:1004,
+    name:"Versace Dominus Watch",
+    brand:"Versace",
+    category:"Watches",
+    price:"₹4,000",
+    image:"Versace Dominus Skeleton Watch 3.jpeg",
+    link:"product-details.html?id=3"
+},
 
+{
+    id:1005,
+    name:"Rolex Daytona 116578SACO",
+    brand:"Rolex",
+    category:"Watches",
+    price:"₹3,500",
+    video:"Rolex Daytona 116578SACO.mp4",
+    link:"product-details.html?id=4"
+},
+
+{
+    id:1006,
+    name:"Tommy Hilfiger Clark",
+    brand:"Tommy Hilfiger",
+    category:"Watches",
+    price:"₹3,500",
+    image:"Tommy Hilfiger Clark.jpeg",
+    link:"product-details.html?id=5"
+},
+
+{
+    id:1007,
+    name:"Omega Speedmaster James Bond",
+    brand:"Omega",
+    category:"Watches",
+    price:"₹4,000",
+    video:"Omega Speedmaster James Bond.mp4",
+    link:"product-details.html?id=6"
+},
+
+{
+    id:1008,
+    name:"Fossil Ring Watch",
+    brand:"Fossil",
+    category:"Watches",
+    price:"₹4,000",
+    video:"Fossil Ring watch.mp4",
+    link:"product-details.html?id=73"
+},
+
+{
+    id:1009,
+    name:"Versace Hellenyium Chrono Watch",
+    brand:"Versace",
+    category:"Watches",
+    price:"₹4,000",
+    image:"Versace Hellenyium Chrono Watch.jpeg",
+    link:"product-details.html?id=7"
+},
+
+{
+    id:1010,
+    name:"Tommy Hilfiger Legend Series",
+    brand:"Tommy Hilfiger",
+    category:"Watches",
+    price:"₹4,000",
+    video:"Tommy Hilfiger Legend Series.mp4",
+    link:"product-details.html?id=8"
+},
+
+{
+    id:1011,
+    name:"Emporio Armani AR70010",
+    brand:"Emporio Armani",
+    category:"Watches",
+    price:"₹4,000",
+    video:"Emporio Armani AR70010.mp4",
+    link:"product-details.html?id=72"
+},
+
+{
+    id:1012,
+    name:"Tommy Hilfiger Dual Tone Watch TH1781644",
+    brand:"Tommy Hilfiger",
+    category:"Watches",
+    price:"₹3,500",
+    video:"Tommy Hilfiger Dual Tone Watch TH1781644.mp4",
+    link:"product-details.html?id=9"
+},
+
+{
+    id:1013,
+    name:"Versace Tonneau Skeleton Watch",
+    brand:"Versace",
+    category:"Watches",
+    price:"₹4,000",
+    image:"Versace Tonneau Skeleton Watch.jpeg",
+    link:"product-details.html?id=10"
+},
+
+{
+    id:1014,
+    name:"Guess Collection Watch",
+    brand:"Guess",
+    category:"Watches",
+    price:"₹4,000",
+    video:"Guess Collection watch.mp4",
+    link:"product-details.html?id=74"
+},
+
+{
+    id:1015,
+    name:"Seiko 5 Sports Red Blood",
+    brand:"Seiko",
+    category:"Watches",
+    price:"₹4,500",
+    video:"Seiko 5 Sports Red Blood.mp4",
+    link:"product-details.html?id=11"
+},
+
+{
+    id:1016,
+    name:"Guess Edge GW0539G1",
+    brand:"Guess",
+    category:"Watches",
+    price:"₹4,000",
+    video:"Guess Edge GW0539G1.mp4",
+    link:"product-details.html?id=75"
+},
+
+{
+    id:1017,
+    name:"Omega Seamaster Diver 300M Co-Axial Master",
+    brand:"Omega",
+    category:"Watches",
+    price:"₹3,500",
+    video:"Omega Seamaster Diver 300M Co-Axial Master.mp4",
+    link:"product-details.html?id=12"
+},
+
+{
+    id:1018,
+    name:"Casio Edifice EFR-539SG-1AUDF",
+    brand:"Casio",
+    category:"Watches",
+    price:"₹3,000",
+    video:"Casio Edifice EFR-539SG-1AUDF.mp4",
+    link:"product-details.html?id=13"
+},
+
+{
+    id:1019,
+    name:"Guess Jet Multifunctional Watch",
+    brand:"Guess",
+    category:"Watches",
+    price:"₹4,500",
+    video:"Guess Jet Multifunctional Watch.mp4",
+    link:"product-details.html?id=77"
+},
+
+{
+    id:1020,
+    name:"Tommy Hilfiger Model 1792196",
+    brand:"Tommy Hilfiger",
+    category:"Watches",
+    price:"₹4,000",
+    video:"Tommy Hilfiger Model 1792196.mp4",
+    link:"product-details.html?id=14"
+},
+
+{
+    id:1021,
+    name:"Rolex Datejust 41 Wimbledon Dial",
+    brand:"Rolex",
+    category:"Watches",
+    price:"₹3,500",
+    video:"Rolex Datejust 41 Wimbledon Dial.mp4",
+    link:"product-details.html?id=15"
+},
+
+{
+    id:1022,
+    name:"Rolex Cosmograph Daytona John Mayer 116508",
+    brand:"Rolex",
+    category:"Watches",
+    price:"₹3,500",
+    video:"Rolex Cosmograph Daytona John Mayer 116508.mp4",
+    link:"product-details.html?id=16"
+},
+
+{
+    id:1023,
+    name:"Guess GW0984G4",
+    brand:"Guess",
+    category:"Watches",
+    price:"₹4,000",
+    video:"Guess GW0984G4.mp4",
+    link:"product-details.html?id=76"
+},
+
+{
+    id:1024,
+    name:"Seiko Presage Open Heart Collection",
+    brand:"Seiko",
+    category:"Watches",
+    price:"₹4,500",
+    video:"Seiko Presage Open Heart Collection.mp4",
+    link:"product-details.html?id=82"
+},
+
+{
+    id:1025,
+    name:"Breitling Navitimer",
+    brand:"Breitling",
+    category:"Watches",
+    price:"₹6,000",
+    video:"Breitling Navitimer .mp4",
+    link:"product-details.html?id=17"
+},
+
+{
+    id:1026,
+    name:"Tommy Hilfiger Henry Skeleton Automatic Watch",
+    brand:"Tommy Hilfiger",
+    category:"Watches",
+    price:"₹4,000",
+    video:"Tommy Hilfiger Henry Skeleton Automatic Watch.mp4",
+    link:"product-details.html?id=18"
+},
+
+{
+    id:1027,
+    name:"Guess Camden Multifunction GW1051G2",
+    brand:"Guess",
+    category:"Watches",
+    price:"₹3,500",
+    image:"Guess Camden Multifunction GW1051G2 1.jpeg",
+    link:"product-details.html?id=19"
+},
+
+{
+    id:1028,
+    name:"Seiko Presage SRPE15",
+    brand:"Seiko",
+    category:"Watches",
+    price:"₹5,500",
+    image:"Seiko Presage SRPE15.jpeg",
+    link:"product-details.html?id=20"
+},
+
+{
+    id:1029,
+    name:"Tommy Hilfiger Baker Watch",
+    brand:"Tommy Hilfiger",
+    category:"Watches",
+    price:"₹3,500",
+    video:"Tommy Hilfiger Baker Watch.mp4",
+    link:"product-details.html?id=21"
+},
+
+{
+    id:1030,
+    name:"Guess Pheonix GW0203G3",
+    brand:"Guess",
+    category:"Watches",
+    price:"₹4,000",
+    video:"Guess Pheonix GW0203G3.mp4",
+    link:"product-details.html?id=78"
+},
+
+{
+    id:1031,
+    name:"Guess-3D Dragon Watch",
+    brand:"Guess",
+    category:"Watches",
+    price:"₹4,000",
+    video:"Guess-3D Dragon Watch.mp4",
+    link:"product-details.html?id=79"
+},
+
+{
+    id:1032,
+    name:"Seiko Redshift SSH137J1",
+    brand:"Seiko",
+    category:"Watches",
+    price:"₹4,000",
+    video:"Seiko Redshift SSH137J1.mp4",
+    link:"product-details.html?id=83"
+},
+
+{
+    id:1033,
+    name:"Seiko 5 Sports GMT SSK035K1",
+    brand:"Seiko",
+    category:"Watches",
+    price:"₹4,000",
+    image:"Seiko 5 Sports GMT SSK035K1.jpeg",
+    link:"product-details.html?id=22"
+},
+
+{
+    id:1034,
+    name:"Seiko 5 Sports Quartz GMT SSK035K1",
+    brand:"Seiko",
+    category:"Watches",
+    price:"₹3,500",
+    image:"Seiko 5 Sports Quartz GMT SSK035K1.jpeg",
+    link:"product-details.html?id=23"
+},
+
+{
+    id:1035,
+    name:"Seiko Prospex Speedtimer SPB513",
+    brand:"Seiko",
+    category:"Watches",
+    price:"₹3,500",
+    image:"Seiko Prospex Speedtimer SPB513.jpeg",
+    link:"product-details.html?id=24"
+},
+
+{
+    id:1036,
+    name:"Seiko 5 Sports Group",
+    brand:"Seiko",
+    category:"Watches",
+    price:"₹4,000",
+    video:"Seiko 5 Sports Group.mp4",
+    link:"product-details.html?id=25"
+},
+
+{
+    id:1037,
+    name:"Tag Heuer Carrera 160 Year Anniversary Limited Edition",
+    brand:"Tag Heuer",
+    category:"Watches",
+    price:"₹4,500",
+    video:"Tag Heuer Carrera 160 Year Anniversary Limited Edition.mp4",
+    link:"product-details.html?id=84"
+},
+
+{
+    id:1038,
+    name:"Seiko Presage Cocktail SRPG23J1",
+    brand:"Seiko",
+    category:"Watches",
+    price:"₹4,000",
+    video:"Seiko Presage Cocktail SRPG23J1.mp4",
+    link:"product-details.html?id=26"
+},
+
+{
+    id:1039,
+    name:"Armani Exchange Automatic Skeleton",
+    brand:"Armani Exchange",
+    category:"Watches",
+    price:"₹4,000",
+    image:"Armani Exchange Automatic Skeleton.jpeg",
+    link:"product-details.html?id=27"
+},
+
+{
+    id:1040,
+    name:"Tudor Pelagos 39",
+    brand:"Tudor",
+    category:"Watches",
+    price:"₹5,000",
+    video:"Tudor Pelagos 39.mp4",
+    link:"product-details.html?id=28"
+},
+
+{
+    id:1041,
+    name:"Seiko Mod Watches",
+    brand:"Seiko",
+    category:"Watches",
+    price:"₹5,500",
+    image:"Seiko Mod Watches.jpeg",
+    link:"product-details.html?id=29"
+},
+
+{
+    id:1042,
+    name:"Guess Arc Champagne",
+    brand:"Guess",
+    category:"Watches",
+    price:"₹4,000",
+    image:"Guess Arc Champagne.jpeg",
+    link:"product-details.html?id=30"
+},
+
+{
+    id:1043,
+    name:"Richard Mille RM055",
+    brand:"Richard Mille",
+    category:"Watches",
+    price:"₹4,000",
+    video:"Richard Mille RM055.mp4",
+    link:"product-details.html?id=80"
+},
+
+{
+    id:1044,
+    name:"Rolex Yatch-Master 42",
+    brand:"Rolex",
+    category:"Watches",
+    price:"₹5,000",
+    image:"Rolex Yatch-Master 42.jpeg",
+    link:"product-details.html?id=31"
+},
+
+{
+    id:1045,
+    name:"Seiko Blue Moon SSA347J1",
+    brand:"Seiko",
+    category:"Watches",
+    price:"₹6,000",
+    image:"Seiko Blue Moon SSA347J1.jpeg",
+    link:"product-details.html?id=32"
+},
+
+{
+    id:1046,
+    name:"Seiko Mod",
+    brand:"Seiko",
+    category:"Watches",
+    price:"₹5,000",
+    image:"Seiko Mod .jpeg",
+    link:"product-details.html?id=33"
+},
+
+{
+    id:1047,
+    name:"Rolex GMT-Master II Root Beer",
+    brand:"Rolex",
+    category:"Watches",
+    price:"₹3,500",
+    video:"Rolex GMT-Master II Root Beer.mp4",
+    link:"product-details.html?id=34"
+},
+
+{
+    id:1048,
+    name:"Seiko Astron GPS Watch",
+    brand:"Seiko",
+    category:"Watches",
+    price:"₹4,000",
+    video:"Seiko Astron GPS Watch.mp4",
+    link:"product-details.html?id=81"
+},
+
+{
+    id:1049,
+    name:"Seiko Mod Yatch-Master Rose Gold",
+    brand:"Seiko",
+    category:"Watches",
+    price:"₹5,000",
+    video:"Seiko Mod Yatch-Master Rose Gold.mp4",
+    link:"product-details.html?id=35"
+},
+
+{
+    id:1050,
+    name:"Guess Waygene",
+    brand:"Guess",
+    category:"Watches",
+    price:"₹3,500",
+    image:"Guess Waygene.jpeg",
+    link:"product-details.html?id=36"
+},
+
+{
+    id:1051,
+    name:"Seiko Pepsi Edition",
+    brand:"Seiko",
+    category:"Watches",
+    price:"₹6,000",
+    image:"Seiko Pepsi Edition.jpeg",
+    link:"product-details.html?id=37"
+},
+
+{
+    id:1052,
+    name:"Cartier 21L 71-6-151",
+    brand:"Cartier",
+    category:"Watches",
+    price:"₹6,000",
+    video:"Cartier 21L 71-6-151.mp4",
+    link:"product-details.html?id=38"
+},
+
+{
+    id:1053,
+    name:"Swatch X Audemars Piguet Royal Pop",
+    brand:"Swatch X Audemars Piguet",
+    category:"Watches",
+    price:"₹4,000",
+    image:"Swatch X Audemars Piguet Royal Pop.jpeg",
+    link:"product-details.html?id=39"
+},
+
+{
+    id:1054,
+    name:"Seiko Mod Watches",
+    brand:"Seiko",
+    category:"Watches",
+    price:"₹4,500",
+    video:"Seiko Mod Watches Video.mp4",
+    link:"product-details.html?id=40"
+},
+
+{
+    id:1055,
+    name:"Tag Heuer Grand Carrera CR7",
+    brand:"Tag Heuer",
+    category:"Watches",
+    price:"₹4,000",
+    image:"Tag Heuer Grand Carrera CR7 .jpeg",
+    link:"product-details.html?id=41"
+},
+
+{
+    id:1056,
+    name:"Audemars Piguet Royal Oak",
+    brand:"Audemars Piguet",
+    category:"Watches",
+    price:"₹6,000",
+    image:"Audemars Piguet Royal Oak 1.jpeg",
+    link:"product-details.html?id=42"
+},
+
+{
+    id:1057,
+    name:"Bannon Automatic Analog (ME3207)",
+    brand:"Bannon",
+    category:"Watches",
+    price:"₹4,500",
+    image:"Bannon Automatic Analog (ME3207).jpeg",
+    link:"product-details.html?id=43"
+},
+
+{
+    id:1058,
+    name:"Armani Exchange Hampton AX2418",
+    brand:"Armani Exchange",
+    category:"Watches",
+    price:"₹4,500",
+    image:"Armani Exchange Hampton AX2418.jpeg",
+    link:"product-details.html?id=44"
+},
+
+{
+    id:1059,
+    name:"Seiko Datejust Mod",
+    brand:"Seiko",
+    category:"Watches",
+    price:"₹5,000",
+    video:"Seiko Datejust Mod.mp4",
+    link:"product-details.html?id=45"
+},
+
+{
+    id:1060,
+    name:"Casio G-Shock",
+    brand:"Casio",
+    category:"Watches",
+    price:"₹4,000",
+    image:"Casio G-Shock 1.jpeg",
+    link:"product-details.html?id=46"
+},
+
+{
+    id:1061,
+    name:"Tommy Hilfiger TW001863-001",
+    brand:"Tommy Hilfiger",
+    category:"Watches",
+    price:"₹4,000",
+    image:"Tommy Hilfiger TW001863-001.jpeg",
+    link:"product-details.html?id=47"
+},
+
+{
+    id:1062,
+    name:"Jacob & Co CR7 Watch",
+    brand:"Jacob & Co",
+    category:"Watches",
+    price:"₹4,000",
+    image:"Jacob & Co CR7 Watch.jpeg",
+    link:"product-details.html?id=48"
+},
+
+{
+    id:1063,
+    name:"Omega X Swatch Moon's Watch",
+    brand:"Omega X Swatch",
+    category:"Watches",
+    price:"₹4,000",
+    image:"Omega X Swatch Moon's Watch.jpeg",
+    link:"product-details.html?id=49"
+},
+
+{
+    id:1064,
+    name:"Casio Edifice ECB-2000DD",
+    brand:"Casio",
+    category:"Watches",
+    price:"₹6,000",
+    image:"Casio Edifice ECB-2000DD.jpeg",
+    link:"product-details.html?id=50"
+},
+
+{
+    id:1065,
+    name:"Emporio Armani Luigi AR1808",
+    brand:"Emporio Armani",
+    category:"Watches",
+    price:"₹4,500",
+    image:"Emporio Armani Luigi AR1808.jpeg",
+    link:"product-details.html?id=51"
+},
+
+{
+    id:1066,
+    name:"Patek Philippe Nautilus Chain",
+    brand:"Patek Philippe",
+    category:"Watches",
+    price:"₹6,500",
+    image:"Patek Philippe Nautilus Chain 1.jpeg",
+    link:"product-details.html?id=52"
+},
+
+{
+    id:1067,
+    name:"Longiness Master Collection",
+    brand:"Longiness",
+    category:"Watches",
+    price:"₹4,000",
+    image:"Longiness Master Collection.jpeg",
+    link:"product-details.html?id=53"
+},
+
+{
+    id:1068,
+    name:"Patek Philippe Nautilus Band",
+    brand:"Patek Philippe",
+    category:"Watches",
+    price:"₹5,000",
+    image:"Patek Philippe Nautilus Band.jpeg",
+    link:"product-details.html?id=54"
+},
+
+{
+    id:1069,
+    name:"Maserati R8853100027 Competizione Plain",
+    brand:"Maserati",
+    category:"Watches",
+    price:"₹5,000",
+    image:"Maserati R8853100027 Competizione Plain.jpeg",
+    link:"product-details.html?id=55"
+},
+
+{
+    id:1070,
+    name:"Emporio Armani BSF954",
+    brand:"Emporio Armani",
+    category:"Watches",
+    price:"₹4,000",
+    image:"Emporio Armani BSF954.jpeg",
+    link:"product-details.html?id=56"
+},
+
+{
+    id:1071,
+    name:"Seiko Presage Cocktail Time",
+    brand:"Seiko",
+    category:"Watches",
+    price:"₹4,500",
+    image:"Seiko Presage Cocktail Time.jpeg",
+    link:"product-details.html?id=57"
+},
+
+{
+    id:1072,
+    name:"Seiko Prospex Diver",
+    brand:"Seiko",
+    category:"Watches",
+    price:"₹4,000",
+    video:"Seiko Prospex Diver .mp4",
+    link:"product-details.html?id=58"
+},
+
+{
+    id:1073,
+    name:"Emporio Armani Ceramica",
+    brand:"Emporio Armani",
+    category:"Watches",
+    price:"₹4,000",
+    image:"Emporio Armani Ceramica.jpeg",
+    link:"product-details.html?id=59"
+},
+
+{
+    id:1074,
+    name:"Seiko SRPL64",
+    brand:"Seiko",
+    category:"Watches",
+    price:"₹4,500",
+    image:"Seiko SRPL64.jpeg",
+    link:"product-details.html?id=60"
+},
+
+{
+    id:1075,
+    name:"Rolex Cosmograph",
+    brand:"Rolex",
+    category:"Watches",
+    price:"₹5,000",
+    image:"Rolex Cosmograph .jpeg",
+    link:"product-details.html?id=61"
+},
+
+{
+    id:1076,
+    name:"Emporio Armani Luigi AR187",
+    brand:"Emporio Armani",
+    category:"Watches",
+    price:"₹4,000",
+    image:"Emporio Armani Luigi AR187.jpeg",
+    link:"product-details.html?id=62"
+},
+
+{
+    id:1077,
+    name:"Rolex Cosmograph Daytona Green",
+    brand:"Rolex",
+    category:"Watches",
+    price:"₹5,000",
+    image:"Rolex Cosmograph Dayona Green.jpeg",
+    link:"product-details.html?id=63"
+},
+
+{
+    id:1078,
+    name:"Tissot Carson",
+    brand:"Tissot",
+    category:"Watches",
+    price:"₹4,500",
+    image:"Tissot Carson.jpeg",
+    link:"product-details.html?id=64"
+},
+
+{
+    id:1079,
+    name:"Rolex Cosmograph Daytona John Walker",
+    brand:"Rolex",
+    category:"Watches",
+    price:"₹5,000",
+    image:"Rolex Cosmograph Daytona John Walker.jpeg",
+    link:"product-details.html?id=65"
+},
+
+{
+    id:1080,
+    name:"Rolex Daytona Rainbow",
+    brand:"Rolex",
+    category:"Watches",
+    price:"₹5,000",
+    image:"Rolex Daytona Rainbow.jpeg",
+    link:"product-details.html?id=66"
+},
+
+{
+    id:1081,
+    name:"Tissot Gentleman Powermatic 80",
+    brand:"Tissot",
+    category:"Watches",
+    price:"₹5,000",
+    image:"Tissot Gentelman Powermatic 80.jpeg",
+    link:"product-details.html?id=67"
+},
+
+{
+    id:1082,
+    name:"Tommy Hilfiger Automatic Skeleton Watch",
+    brand:"Tommy Hilfiger",
+    category:"Watches",
+    price:"₹4,500",
+    image:"Tommy Hilfiger Automatic Skeleton Watch 1.jpeg",
+    link:"product-details.html?id=68"
+},
+
+{
+    id:1083,
+    name:"Casio Edifice EFR-539SG-1AVUDF",
+    brand:"Casio",
+    category:"Watches",
+    price:"₹3,500",
+    image:"Casio Edifice EFR-539SG-1AVUDF 1.jpeg",
+    link:"product-details.html?id=69"
+},
+
+{
+    id:1084,
+    name:"Tissot PRX",
+    brand:"Tissot",
+    category:"Watches",
+    price:"₹4,000",
+    image:"Tissot PRX 1.jpeg",
+    link:"product-details.html?id=70"
+}
+    
     /* =====================================================
        COUPLE WATCHES
     ===================================================== */
