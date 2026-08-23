96db471bd31f46486ebdcc07f12f33e97c1e7323
50mirars-bit <50mirars@gmail.com>
2026-08-23 11:54:03 +0530

    Create search-products.js

---
+++ search-products.js
@@ -0,0 +1,43 @@
+const searchProducts = [
+
+    {
+        id: 1,
+        name: "Cartier C Decor Sunglasses",
+        brand: "Cartier",
+        category: "Sunglasses",
+        price: "₹2,500",
+        image: "Cartier C Decor.mp4",
+        link: "product-detailssunglasses.html?id=1"
+    },
+
+    {
+        id: 2,
+        name: "Cartier CT06010 Sunglasses",
+        brand: "Cartier",
+        category: "Sunglasses",
+        price: "₹2,500",
+        image: "Cartier CT06010.mp4",
+        link: "product-detailssunglasses.html?id=2"
+    },
+
+    {
+        id: 3,
+        name: "Balmain Paris 7020 Purple Shade Sunglasses",
+        brand: "Balmain",
+        category: "Sunglasses",
+        price: "₹2,500",
+        image: "Balmain Paris 7020 Purple Shade Sunglasses.jpeg",
+        link: "product-detailssunglasses.html?id=3"
+    },
+
+    {
+        id: 4,
+        name: "Ray-Ban Meta Gen-2",
+        brand: "Ray-Ban",
+        category: "Sunglasses",
+        price: "₹16,000",
+        image: "Ray-Ban Meta Gen-2.jpeg",
+        link: "product-detailssunglasses.html?id=4"
+    }
+
+];
